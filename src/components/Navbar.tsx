5a5e28d854793827e456a2244bd8a98757a8618f
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-08-11 11:07:03 +0500

    react-typescript-todo

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='Navbar'>
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <h1>Navbar</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
